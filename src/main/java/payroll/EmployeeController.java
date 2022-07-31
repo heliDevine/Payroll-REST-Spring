609813261845d98a7ea54aaa5fba9f722e1dc2c9
Heli Devine <heli.devine@gmail.com>
2022-07-31 18:43:45 +0100

    trying to fix bugs

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -1,34 +1,40 @@
 package payroll;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-public class EmployerController {
+public class  EmployeeController {
 
     private final EmployeeRepository repository;
-    EmployerController(EmployeeRepository repository) {
+    EmployeeController(EmployeeRepository repository) {
         this.repository = repository;
     }
+
+    // Aggregate root
+    // tag::get-aggregate-root[]
     @GetMapping("/employees")
+    List<Employee> all() {
+        return repository.findAll();
+    }
+    // end::get-aggregate-root[]
+
+    @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee) {
         return repository.save(newEmployee);
     }
-    @GetMapping("/employees{id}")
+
+
+    @GetMapping("/employees/{id}")
     Employee one(@PathVariable Long id) {
+
         return repository.findById(id)
-                .orElseThrow(()->new EmployeeNotFoundException(id));
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
     }
+
+
     @PutMapping("/employees{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
         return repository.findById(id)
--- src/main/java/payroll/EmployeeNotFoundAdvise.java
+++ src/main/java/payroll/EmployeeNotFoundAdvise.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-class EmployeeNotFoundAdvise {
+class EmployeeNotFoundAdvice {
+
     @ResponseBody
     @ExceptionHandler(EmployeeNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
